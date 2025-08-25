fd2838abb0d34b7edf51198c6db9189c59ad721b
Rodrigo Goncalves <rodrigo.g.459@gmail.com>
2025-08-25 18:59:31 -0300

    Adicionndo configuração inicial do servidor Nginx e a configuração do worker com Python

---
+++ worker/worker.py
@@ -0,0 +1,20 @@
+import redis
+import json
+import os
+from time import sleep
+from random import randint
+
+
+if __name__ == '__main__':
+    redis_host = os.getenv('REDIS_HOST')
+    redis_port = int(os.getenv('REDIS_PORT'))
+    r = redis.Redis(host=redis_host, port=redis_port, db=0)
+
+    print('Aguardando mensagens...')
+
+    while True:
+        mensagem = json.loads(r.blpop('sender')[1])
+        # Simulando envio de e-mail..
+        print('Mandando a mensagem:', mensagem['assunto'])
+        sleep(randint(15, 45))
+        print('Mensagem', mensagem['assunto'], 'enviada.')
